8b1e136a5097a7cf72c790cd8813e3e3cb1a74d3
aliakborswe <aliakborswe@gmail.com>
2025-04-14 12:37:14 +0600

    connect to sql lite db and get all categories

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
+
+// Connect to SQLite database
+const db = new sqlite3.Database("./dua_main.sqlite", (err) => {
+  if (err) return console.error(err.message);
+  console.log("Connected to SQLite database.");
+});
+
+// Get all categories
+app.get("/api/categories", (req, res) => {
+  db.all("SELECT * FROM category", [], (err, rows) => {
+    if (err) return res.status(500).send(err.message);
+    res.json(rows);
+  });
+});
 
 
 app.listen(PORT, () =>
